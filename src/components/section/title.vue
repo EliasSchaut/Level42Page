ce2287ff57033db46f18c637166256e87a87af04
EliasSchaut <eschaut@outlook.de>
2024-08-28 20:52:26 +0200

    fill the whole main page + imprint + robots.txt

---
+++ src/components/section/title.vue
@@ -0,0 +1,7 @@
+<template>
+  <h2
+    class="mb-8 mt-16 select-none bg-gradient-to-b from-amber-400 to-amber-600 bg-clip-text text-center text-4xl font-bold text-transparent"
+  >
+    <slot />
+  </h2>
+</template>
---
+++ src/pages/imprint.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="m-10">
+    <h1 class="mb-4 text-2xl font-bold">Impressum</h1>
+    Angaben gemäß § 5 TMG<br />
+    <p class="my-2">
+      Elias Lorenz Schaut<br />
+      Klosterweg 28<br />
+      76131 Karlsruhe<br />
+    </p>
+
+    <p>
+      E-Mail:
+      <a class="text-blue-300 underline" href="mailto:elias@schaut.dev"
+        >elias@schaut.dev</a
+      >
+    </p>
+  </div>
+</template>
+<script setup lang="ts"></script>
