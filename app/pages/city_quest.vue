8b3d9f3efa3fb2dbb383a5ea80baaf1dc1bfd1df
EliasSchaut <eschaut@outlook.de>
2025-10-18 18:40:16 +0200

    update city_quest.vue

--- app/pages/city_quest.vue
+++ app/pages/city_quest.vue
@@ -3,11 +3,11 @@
     class="m-5 flex flex-col justify-start gap-4 sm:items-start"
     @submit.prevent="check_solution"
   >
-    <h1 class="text-xl font-semibold text-prime-600 dark:text-prime-500">
+    <h1 class="text-prime-600 dark:text-prime-500 text-xl font-semibold">
       Gib eine Lösung ein:
     </h1>
     <input
-      class="block rounded-md border-0 px-3 py-1.5 text-second-900 shadow-sm ring-1 ring-inset ring-second-300 placeholder:text-second-400 dark:bg-second-700 dark:text-white dark:ring-second-600 dark:placeholder:text-second-400 sm:text-sm sm:leading-6"
+      class="text-second-900 ring-second-300 placeholder:text-second-400 dark:bg-second-700 dark:ring-second-600 dark:placeholder:text-second-400 block rounded-md border-0 px-3 py-1.5 shadow-sm ring-1 ring-inset sm:text-sm sm:leading-6 dark:text-white"
       placeholder="solution"
       type="text"
       name="guess"
@@ -15,14 +15,14 @@
       required
     />
     <button
-      class="rounded-md bg-prime-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-prime-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-prime-600"
+      class="bg-prime-600 hover:bg-prime-500 focus-visible:outline-prime-600 rounded-md px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
       type="submit"
     >
       Prüfen
     </button>
   </form>
 
-  <div class="m-5" v-html="guess"></div>
+  <div class="m-5 h-full" v-html="guess"></div>
 </template>
 
 <script lang="ts">
